17e3cd8f7c47f613534401baf4c59444623fa839
jetliOS <jesherrera2001@gmail.com>
2024-07-19 23:57:54 -0500

    CollectionView updates

---
+++ Anime Shop/HomeCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  HomeCollectionViewCell.swift
+//  Anime Shop
+//
+//  Created by Jesus Herrera on 19/07/24.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Anime Shop/ViewController.swift
+++ Anime Shop/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.presentHomeViewController()
+                }
     }
 
+    func presentHomeViewController() {
+            // Crea una instancia del storyboard que contiene HomeViewController
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            
+            // Crea una instancia de HomeViewController usando su identificador
+            if let homeViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController") as? HomeViewController {
+                // Presenta HomeViewController
+                homeViewController.modalPresentationStyle = .fullScreen
+                self.present(homeViewController, animated: true, completion: nil)
+            }
+        }
 
 }
 
